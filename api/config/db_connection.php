322fe0ec9aac15563071735f906e172b834a2c63
Ang3llo55 <angelloalejandro_2002@hotmail.com>
2024-10-02 22:57:34 -0500

    Conexion a la base de datos

---
+++ api/config/db_connection.php
@@ -0,0 +1,38 @@
+<?php
+/*$numero1 =20;
+
+$numero2 =30;
+$addition = $numero1 + $numero2;
+echo $addition;
+
+*/
+
+$host = "localhost";
+$port = "5432"; 
+$dbname = "beta";
+$username = "postgres";
+$password = "1234";
+
+//crear variables de conectividad
+$data_connection = "
+host = $host
+port = $port
+dbname = $dbname
+user = $username
+password = $password";
+
+$conn =pg_connect($data_connection);
+
+    //verificar conectividad
+    if (!$conn) {
+        die("Connection failed: ". pg_last_error());
+    }
+    else {echo "Connection successfully";
+    }
+    
+    
+    //cerrar la conexion
+
+    pg_close($conn);
+
+?>
