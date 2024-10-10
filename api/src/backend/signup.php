f73946105d28bf9a67977354f62a052c0aae284d
Ang3llo55 <angelloalejandro_2002@hotmail.com>
2024-10-10 11:22:25 -0500

    Agregar validacion de email

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -6,6 +6,17 @@
     $pass = $_POST['pass'];
     //Encrypt password with md5 hashing algorithm
     $enc_pass = md5($pass);
+
+    //Validate if email alredy exists
+    $query = "SELECT * FROM users WHERE email = '$email'";
+    $result = pg_query($conn, $query);
+    $row = pg_fetch_assoc($result);
+    if ($row){
+        echo "<script>alert('Email already exists!')</script>";
+        header('refresh:0;url=http://127.0.0.1/Beta/api/src/register_form.html');
+        exit();
+    
+    }
     //Query to insert data into users table
     $query = "INSERT INTO users (email,password)
         VALUES ('$email', '$enc_pass');
@@ -16,8 +27,8 @@
 
     if ($result) {
         //echo "<br>Registration successful!";
-        echo"<script>alert('Registration successful!')</script>";
-            header('refresh:0;url=http://127.0.0.1/Beta/api/src/login_form.html');
+        echo "<script>alert('Registration successful!')</script>";
+        header('refresh:0;url=http://127.0.0.1/Beta/api/src/login_form.html');
     } else {
         echo "Registration failed!";
     }
