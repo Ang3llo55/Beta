07621f8dc831673c64bf065fb1fa86655fc4d85e
Ang3llo55 <angelloalejandro_2002@hotmail.com>
2024-10-17 22:41:21 -0500

    se copia el codigo del sign up y se ajusta para el sign in

---
+++ api/src/backend/signin.php
@@ -0,0 +1,33 @@
+<?php
+require('../../config/db_connection.php');
+
+$email = $_POST['email'];
+$pass = $_POST['password'];
+$enc_pass = md5($pass);
+
+$query = "SELECT * FROM users WHERE email = '$email'";
+$result = pg_query($conn, $query);
+$row = pg_fetch_assoc($result);
+if ($row) {
+    $query = "SELECT * FROM users WHERE password='$enc_pass'";
+    $result = pg_query($conn, $query);
+    $row = pg_fetch_assoc($result);
+    if ($row) {
+        echo "<script>alert('correct')</script>";
+    } else {
+        echo "<script>alert('password incorrect')</script>";
+    }
+    header ('refresh:0; url=http://127.0.0.1/beta/api/src/login_form.html');
+    exit();
+}
+
+$result = pg_query($conn, $query);
+if ($result) {
+    echo "<script>alert('No registration')</script>";
+    header ('refresh:0; url=http://127.0.0.1/beta/api/src/login_form.html');
+} else {
+    echo "Registration failed!";
+}
+pg_close($conn)
+
+?>
