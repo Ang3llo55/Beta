c8d92fbabfbac9566f2801b8b57e2b04c17a1571
Ang3llo55 <angelloalejandro_2002@hotmail.com>
2024-11-01 11:15:05 -0500

    Puliendo codigo

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -3,7 +3,7 @@
         //Supabase database configuration
         $SUPABASE_URL = 'https://ylqvlnwghtqljivoswsb.supabase.co';
         $SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlscXZsbndnaHRxbGppdm9zd3NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzODg2OTksImV4cCI6MjA0NTk2NDY5OX0.nturyTBExOMbHo12740LDch6_NPaTI9qS_yutyLoO7Y';
-        $url = "$SUPABASE_URL/rest/v1/users/";
+        $url = "$SUPABASE_URL/rest/v1/users";
         $data = [
             'email' => $email,
             'password' => $passwd
@@ -29,9 +29,9 @@
          }
          echo "User has been created."; //. json_encode($response_data);
          
-}
+    }
     //DB connection
-    require('../../config/db_connection.php');
+    require "../../config/db_connection.php";
     //Get data from register form
     $email = $_POST['email'];
     $pass = $_POST['pass'];
@@ -42,7 +42,7 @@
     $row = pg_fetch_assoc($result);
     if ($row){
         echo "<script>alert('Email already exists!')</script>";
-        header('refresh:0, url=http://127.0.0.1/Beta/api/src/register_form.html');
+        header("refresh:0, url=http://127.0.0.1/Beta/api/src/register_form.html");
         exit();
     
     }
@@ -57,11 +57,11 @@
         //echo "<br>Registration successful!";
         save_data_supabase($email, $enc_pass);
         echo "<script>alert('Registration successful!')</script>";
-        header('refresh:0;url=http://127.0.0.1/Beta/api/src/login_form.html');
+        header("refresh:0;url=http://127.0.0.1/Beta/api/src/login_form.html");
     } else {
         echo "Registration failed!";
     }
-    pg_close($conn)
+    pg_close($conn);
     
     /*echo "Email: " . $email;
     echo "<br>Password: " . $pass;
