ca362017ca05a4ef86243d966d2483bd36a2bc9f
Ang3llo55 <angelloalejandro_2002@hotmail.com>
2024-11-01 10:59:06 -0500

    actualizacion codigo

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -5,20 +5,23 @@
         $SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlscXZsbndnaHRxbGppdm9zd3NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzODg2OTksImV4cCI6MjA0NTk2NDY5OX0.nturyTBExOMbHo12740LDch6_NPaTI9qS_yutyLoO7Y';
         $url = "$SUPABASE_URL/rest/v1/users/";
         $data = [
-        'email' => $email,
-        'password' => $passwd
+            'email' => $email,
+            'password' => $passwd
         ];
         $options = [
             'http' => [
-                'header' => "Content-Type: application/
-                json\r\nAuthorization: Bearer $SUPABASE_KEY",
+                'header' => [
+                    "Content-Type: application/json",
+                    "Authorization: Bearer $SUPABASE_KEY",
+                    "apikey: $SUPABASE_KEY",
+                ],
                 'method' => 'POST',
                 'content' => json_encode($data),
             ],
         ];
         $context = stream_context_create($options);
         $response = file_get_contents($url, false. $context);
-        $response_data = json_decode($response, true);
+        // $response_data = json_decode($response, true);
 
         if($response === false) {
             echo "Error: Unable to save data to Supabase";
@@ -32,8 +35,6 @@
     //Get data from register form
     $email = $_POST['email'];
     $pass = $_POST['pass'];
-    //Encrypt password with md5 hashing algorithm
-    $enc_pass = md5($pass);
 
     //Validate if email alredy exists
     $query = "SELECT * FROM users WHERE email = '$email'";
@@ -41,20 +42,20 @@
     $row = pg_fetch_assoc($result);
     if ($row){
         echo "<script>alert('Email already exists!')</script>";
-        header('refresh:0;url=http://127.0.0.1/Beta/api/src/register_form.html');
+        header('refresh:0, url=http://127.0.0.1/Beta/api/src/register_form.html');
         exit();
     
     }
+    //Encrypt password with md5 hashing algorithm
+    $enc_pass = md5($pass);
     //Query to insert data into users table
-    $query = "INSERT INTO users (email,password)
-        VALUES ('$email', '$enc_pass');
-    ";
-
+    $query = "INSERT INTO users (email,password) VALUES ('$email', '$enc_pass')";
     //Execute query
     $result = pg_query($conn, $query);
 
     if ($result) {
         //echo "<br>Registration successful!";
+        save_data_supabase($email, $passwd);
         echo "<script>alert('Registration successful!')</script>";
         header('refresh:0;url=http://127.0.0.1/Beta/api/src/login_form.html');
     } else {
